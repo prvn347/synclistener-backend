9db095dc8c01cc40d9ee439bdcb55fa3d5d0c95c
Your New Username <sahupravin960@gmail.com>
2024-06-24 13:13:35 +0530

    added music search end point

--- src/routes/track.ts
+++ src/routes/track.ts
@@ -1,11 +1,36 @@
+import axios from "axios";
 import { Router, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const router = Router();
 
-router.post("/search", async (req: Request, res: Response) => {
+router.get("/search", async (req: Request, res: Response) => {
   try {
     const query = req.query.q as string;
-  } catch (error) {}
+    console.log(query);
+    if (!query) {
+      return res.status(400).json({ error: "Query parameter is required" });
+    }
+    console.log("YouTube API Key:", process.env.YOUTUBE_API_KEY);
+    const response = await axios.get(
+      "https://www.googleapis.com/youtube/v3/search",
+      {
+        params: {
+          part: "snippet",
+          q: query,
+          key: process.env.YOUTUBE_API_KEY,
+          maxResults: 10,
+        },
+      }
+    );
+
+    res.json(response);
+  } catch (error) {
+    console.error(error); // Log the error for debugging
+    res.status(500).json({ error: "Failed to fetch data from YouTube API" });
+  }
 });
 
 export default router;
