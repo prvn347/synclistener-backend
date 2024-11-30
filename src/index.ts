7cca741ae4ed2e6607da3b7fb6bda426b36fb05b
Pravin Sahu <sahupravin960@gmail.com>
2024-11-30 17:34:10 +0530

    fix spin down

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+
+const url = `https://synclistener-backend.onrender.com/health`;
+
+const interval = 3000;
+
+export function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
